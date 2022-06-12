5e9d8beb7c9f47e30833970ea130b418aabc491c
git pessoal <gabriel.san.diogo@gmail.com>
2022-06-12 01:40:27 -0300

    project setup refactoring

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -4,11 +4,8 @@ async function create(req, res, next) {
     
 
     try {
-        let ret = await CreateEventOnCalendar.create(req);
-        if(ret == undefined) throw Error("Error")
-        
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.status(201).json(ret)
+        CreateEventOnCalendar.create(req, res)
     } catch (error) {
         res.status(500).send(error.message)
         next(error)
